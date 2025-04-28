1b88d26df47e40d2216cddabee2429be8ed5f821
Никита <giveyourcatsomemilk@outlook.com>
2025-04-28 21:23:36 +0300

    Привел основной скрипт к читаемому виду

--- anime.py
+++ anime.py
@@ -5,21 +5,15 @@ from anime_parsers_ru import KodikParser
 # Инициализация парсера
 parser = KodikParser()
 
+# Прописываем искомый тайтл
+title = input("Впиши наименование тайтла: ")
 
-# Функция формирования ссылки на видео-файл
-# def get_anime_link(
-#     get_id, get_id_type="shikomori", get_seria_num="0", get_translation="610"
-# ):
-#     return parser.get_link(
-#         id=get_id,
-#         id_type=get_id_type,
-#         seria_num=get_seria_num,
-#         translation_id=get_translation,
-#     )
-
-
-title = "Наруто"
+# Находим всю информацию по наименованию
 results = parser.search(title=title)
+with open("test.txt", "w", encoding="utf-8") as file:
+    json.dump(results, file, ensure_ascii=False, indent=2)
+
+# Формируем словарь из "results" в читаемый вид
 converted_result = {
     element["title"]: {
         "Год": element["year"],
@@ -32,8 +26,16 @@ converted_result = {
     }
     for element in results
 }
-sorted_result = dict(
-    sorted(converted_result.items(), key=lambda x: (x[1]["Тип"], x[1]["Год"]))
-)
+
+# Сортируем словарь по годам
+sorted_result = dict(sorted(converted_result.items(), key=lambda x: x[1]["Год"]))
+
+# Записываем все в читабельный JSON формат
 with open("anime_list.json", "w", encoding="utf-8") as file:
     json.dump(sorted_result, file, ensure_ascii=False, indent=2)
+
+# Выводим краткую информацию
+with open("anime_list.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+    for key, value in data.items():
+        print(f"{key}:\n-Год: {value["Год"]}\n-Формат: {value["Тип"]}\n")
